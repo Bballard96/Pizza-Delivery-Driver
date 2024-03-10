a9d2ae449a4491e8b2efd1ca5eb43ae10372dffc
Brendan96 <Brendan.ballard96@yahoo.com>
2024-03-10 18:55:42 -0500

    add collision

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -7,4 +7,7 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Yo get outta the way");
     }
+    void onTriggerEnter2D(Collision2D other) {
+        Debug.Log("yes we can see you");
+    }
 }
