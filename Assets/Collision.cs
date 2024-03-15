9c1883f649fb5eff8e24ef1282c82e9138737723
Brendan96 <Brendan.ballard96@yahoo.com>
2024-03-15 08:37:35 -0500

    compiler error

--- Assets/Collision.cs
+++ Assets/Collision.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other) {
+    void OnCollisionEnter2D(Collision2D other) 
+    {
         Debug.Log("Yo get outta the way");
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
-        
-        // if (the thing we trigger is the package)
-        
-        // then print "package picked up" to the console
+    void OnTriggerEnter2D(Collider2D other) 
+    {   
+        if (other.tag == "Package")
+        {
+            Debug.log("Package picked up");
+        }
     }
 }
