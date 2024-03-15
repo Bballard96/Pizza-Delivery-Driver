f068fc013cde6be6bc754f3aac93ae05dfbc3fd3
Brendan96 <Brendan.ballard96@yahoo.com>
2024-03-15 08:27:35 -0500

    change collision to Delivery

---
+++ Assets/Delivery.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Yo get outta the way");
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("yes we can see you");
+    }
+}
