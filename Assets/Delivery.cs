b36b29909afd235f9ff56724613766ed8e7928cf
Brendan96 <Brendan.ballard96@yahoo.com>
2024-03-18 08:13:22 -0500

    add packagecolor and nopackagecolor

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     [SerializeField] float destroyDelay = 0.5f;
+    [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
     bool hasPackage = false;
     private void Start() {
         Debug.Log(hasPackage);
@@ -17,7 +19,7 @@ public class Delivery : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other) 
     {
-        if (other.tag == "Package" && hasPackage == false )
+        if (other.tag == "Package" && !hasPackage)
         {
         Debug.Log("Package picked up");
         hasPackage = true;
